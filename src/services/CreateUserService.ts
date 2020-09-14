b0be7e18a888bca5d1243fc6a9553e46d7fdb21a
Vitor Senise <vitor.senise@gmail.com>
2020-09-14 15:49:55 -0300

    Initial Commit. End of nivel 2

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExist = await usersRepository.findOne({ where: { email } });
+
+    if (checkUserExist) {
+      throw new AppError('Email address already used');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    // Not in the DB, just in the response
+    delete user.password;
+
+    return user;
+  }
+}
+
+export default CreateUserService;
