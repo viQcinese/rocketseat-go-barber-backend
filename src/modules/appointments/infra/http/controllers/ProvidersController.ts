45b56d064ea945dc940349b2d462eeb3c704728b
Vitor Senise <vitor.senise@gmail.com>
2020-10-12 13:18:12 -0300

    small untracked changes

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,6 +1,8 @@
-import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
 export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -12,6 +14,6 @@ export default class ProvidersController {
       user_id,
     });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
